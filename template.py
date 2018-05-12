b550ab25eac0a6f033ea22ac38b4e93163419283
[millan04] <[millan04@luther.edu]>
2018-05-11 22:34:06 -0500

    Got the searh are linked up

--- template.py
+++ template.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, render_template
-import random
-
+from flask import Flask, request, render_template, jsonify
+import googlemaps
 app = Flask(__name__)
 
 users = {"millan04": "andrew", "millaand000": "millan"}
@@ -10,15 +9,28 @@ def login():
 	user_name = request.args.get("user")
 	password = request.args.get("password")
 	not_robot = request.args.get("robot")
-	if (user_name in users) and (users[user_name] == password) and isinstance(not_robot, str):
+	if (user_name in users) and (users[user_name] == password): # and isinstance(not_robot, str):
 		return render_template('display.html')
 	else:
-		return render_template('login.html', message = "Incorrecct Username or Password or user is a Robot")
+		return render_template('login.html', message = "Incorrecct Username or Password")
 
 @app.route('/processform')
 def procform():
+	key = 'AIzaSyCANwUbNqn7nOnqcwUG2p2PX5O0cg7sINE'
+	city = request.args.get("city")
+	state = request.args.get("state")
 
-	return render_template('display.html')
+	full_name = city + ', ' + state
+
+	gm = googlemaps.Client(key = key)
+	geocode_result = gm.geocode(full_name)[0]
+	detail = []
+	detail.append("Formal address: " + geocode_result['formatted_address'])
+	detail.append("Region: "+ geocode_result['address_components'][1]['long_name'])
+	detail.append("Latitude: "+ str(geocode_result['geometry']['location']['lat']))
+	detail.append("Longitude: "+ str(geocode_result['geometry']['location']['lng']))
+
+	return render_template('display.html', location = detail)
 
 @app.route('/newuser')
 def new_user():
